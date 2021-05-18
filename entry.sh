affb223a405e28f6aea2da39139d7cd9538aedf9
Ziv <16467659+ziv1234@users.noreply.github.com>
2021-05-18 15:56:01 +0300

    do not remove logs

--- entry.sh
+++ entry.sh
@@ -3,9 +3,9 @@
 # XXX TBD - no need to do if it already exists?
 mkdir -p /home/alpine/.vnc && x11vnc -storepasswd alpine /home/alpine/.vnc/passwd
 
-nohup /usr/bin/Xvfb :99 -screen 0 $RESOLUTION -ac +extension GLX +render -noreset > /dev/null 2>&1 &
-nohup startxfce4 > /dev/null 2>&1 &
-nohup x11vnc -xkb -noxrecord -noxfixes -noxdamage -display :99 -forever -bg -rfbauth /home/alpine/.vnc/passwd -users alpine -rfbport 5900 > /dev/null 2>&1 &
+nohup /usr/bin/Xvfb :99 -screen 0 $RESOLUTION -ac +extension GLX +render -noreset &
+nohup startxfce4 &
+nohup x11vnc -xkb -noxrecord -noxfixes -noxdamage -display :99 -forever -bg -rfbauth /home/alpine/.vnc/passwd -users alpine -rfbport 5900 &
 
 sleep infinity
 
