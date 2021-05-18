c31f4a6ee7d78a70780a1bb755f7d4d47fd1e071
Ziv <16467659+ziv1234@users.noreply.github.com>
2021-05-18 16:38:48 +0300

    move more to variables

--- entry.sh
+++ entry.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # XXX TBD - no need to do if it already exists?
-mkdir -p /home/alpine/.vnc && x11vnc -storepasswd alpine /home/alpine/.vnc/passwd
+mkdir -p /home/$V_USER/.vnc && x11vnc -storepasswd $VNC_PASSWORD /home/$V_USER/.vnc/passwd
 
 nohup /usr/bin/Xvfb :99 -screen 0 $RESOLUTION -ac +extension GLX +render -noreset &
 nohup startxfce4 &
-nohup x11vnc -xkb -noxrecord -noxfixes -noxdamage -display :99 -forever -bg -rfbauth /home/alpine/.vnc/passwd -users alpine -rfbport 5900 &
+nohup x11vnc -xkb -noxrecord -noxfixes -noxdamage -display :99 -forever -bg -rfbauth /home/$V_USER/.vnc/passwd -users $V_USER -rfbport 5900 &
 
 sleep infinity
 
